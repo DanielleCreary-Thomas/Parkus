947f254800a4d9184fb8c3004962cbb4050f4fde
Danielle <danielle.w.c.thomas@gmail.com>
2024-04-05 16:59:11 -0400

    Adding simple functions to bridge, store and controller

--- Backend/Parkus/bridge.py
+++ Backend/Parkus/bridge.py
@@ -6,29 +6,66 @@ import psycopg2
 # connect to db
 conn = psycopg2.connect("dbname=parkus user=postgres password=notasecret")
 
-#get schedules of group members
-# SELECT u.userid, u.first_name,u.last_name,
-# g.*,s.scheduleid, s.start_time, s.end_time, s.dow
-# FROM schedule_blocks s
-# INNER JOIN users u ON s.userid = u.userid
-# INNER JOIN parking_groups g ON u.groupid = g.groupid
-# ORDER BY g.groupid, u.userid, s.dow;
+
+# def get_group_members(groupid):
+#     """
+#     returns a list of all the members of a group and their schedules
+#     :param groupid: the selected group's id
+#     :return: a list of tuples of a userid, groupid, scheduleid, day of week, start time, and end time
+#     """
+#     with conn.cursor() as cur:
+#         cur.execute('''
+#         SELECT u.userid,
+#         g.groupid,s.scheduleid, s.dow, s.start_time, s.end_time
+#         FROM schedule_blocks s
+#         INNER JOIN users u ON s.userid = u.userid
+#         INNER JOIN parking_groups g ON u.groupid = g.groupid
+#         WHERE g.groupid = %s
+#         ORDER BY g.groupid, u.userid, s.dow;
+#         ''', groupid)
+#     group_data = cur.fetchall()
+#     return group_data
+
+def member_userid_for_group(groupid):
+    """
+    Returns the user id of each member of thr group with selected group id
+    :param groupid: selected group's id
+    :return: list of tuples of the user id
+    """
+    with conn.cursor() as cur:
+        cur.execute('''
+        SELECT u.userid
+        FROM users u
+        WHERE u.groupid = %s
+        ''', groupid)
+        members = cur.fetchall()
+        return members
+
+def member_count_by_groupid(groupid):
+    """
+    returns the number of members in the group with the given ID
+    :param groupid: the selected group's id
+    :return: the number of members
+    """
+    with conn.cursor() as cur:
+        cur.execute('''SELECT COUNT(u.userid) as members, g.groupid
+                        FROM parking_groups g
+                        INNER JOIN users u ON u.groupid = g.groupid
+                        WHERE g.groupid = %s
+                        GROUP BY g.groupid
+                        ORDER BY g.groupid''', groupid)
+        member_count = cur.fetchone()[0]
+        return member_count
 
 
-# get group member count
-# SELECT COUNT(u.userid) as members, g.groupid
-# FROM parking_groups g
-# INNER JOIN users u ON u.groupid = g.groupid
-# GROUP BY g.groupid
-# ORDER BY g.groupid;
-def group_id(groupid):
+def group_by_groupid(groupid):
     """
     Returns the group matching the given id
     :param groupid:the selected group's id
     :return: a dictionary represention of a group
     """
     with conn.cursor() as cur:
-        cur.execute("SELECT * FROM parking_groups WHERE groupid = %s", id)
+        cur.execute("SELECT * FROM parking_groups WHERE groupid = %s", groupid)
         group = cur.fetchall()
         group_dict = {
             "groupid":group[0],
@@ -36,4 +73,39 @@ def group_id(groupid):
             "fullypaid":group[2]
         }
         return group_dict
+
+
+def groups_with_vacancies():
+    """
+    Returns the group IDs of groups with less than 5 members
+    :return: a list of tuples with groupids at 0 and their member count at 1
+    """
+    with conn.cursor() as cur:
+        cur.execute('''
+        SELECT COUNT(u.userid) as members, g.groupid
+        FROM parking_groups g
+        INNER JOIN users u ON u.groupid = g.groupid
+        GROUP BY g.groupid
+        HAVING COUNT(u.userid) < 5
+        ORDER BY g.groupid;
+        ''')
+        groups_and_memebers = cur.fetchall()
+        return groups_and_memebers
+
+
+def schedule_blocks_for_user(userid):
+    """
+    returns the schedule blocks for the given user in the form of a list of tuples
+    :param userid: selected user's id
+    :return: the list of schedule blocks
+    """
+    with conn.cursor() as cur:
+        cur.execute('''
+        SELECT s.scheduleid, s.dow, s.start_time, s.end_time 
+        FROM schedule_blocks s
+        WHERE s.userid = %s
+        ORDER BY dow
+        ''', userid)
+    schedule = cur.fetchall()
+    return schedule
 
--- Backend/Parkus/data_store.py
+++ Backend/Parkus/data_store.py
@@ -1,10 +1,71 @@
 ## business logic
 # actual code of making things work
 import bridge
+
+
+class User:
+    def __init__(self, id):
+        self.id = id
+        self.schedule = []
+
+    def add_schedule(self, schedule_block):
+        self.schedule.append(schedule_block)
+
+    def get_schedule_for_userid(self):
+        user_schedule = bridge.schedule_blocks_for_user(self.id)
+        for block in user_schedule:
+            self.add_schedule(Schedule(block[0], block[1], block[2], block[3]))
+
+
+class Schedule:
+    def __init__(self, id, dow, start, end):
+        self.id = id
+        self.dow = dow
+        self.start_time = start
+        self.end_time = end
+
+    def compare_times(self, member_time):
+        comparison = max(0, min(self.end_time, member_time.end_time) - max(self.start_time, member_time.start_time))
+        return comparison == 0
+
+class Group:
+    def __init__(self, groupid):
+        self.id = groupid
+        self.members = []
+
+    def add_member(self, member):
+        self.members.append(member)
+
+    def populate_group(self):
+        members = bridge.member_userid_for_group(self.id)
+        if len(members) != 0:
+            for userid in members:
+                member = User(userid)
+                member.get_schedule_for_userid()
+                self.add_member(member)
+
+
+
 def get_group_by_id(id):
     """
     returns the group with matching id
     :param id: id of selected group
     :return: group data in the form of a dictionary
     """
-    return bridge.group_id(id)
+    return bridge.group_by_id(id)
+
+
+
+
+
+
+def complete_matchmaking(userid):
+    curr_user = User(userid)
+    curr_user.get_schedule_for_userid()
+    group_options = []
+    available_groups = bridge.groups_with_vacancies()
+
+    if len(available_groups) != 0:
+        for group in available_groups:
+            curr_group = Group(group[0])
+            curr_group.populate_group()
