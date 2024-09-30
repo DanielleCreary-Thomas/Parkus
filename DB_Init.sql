b3ffd3429c5995e859f69e5c9b6790feda98ba1a
Danielle <77177477+DanielleCreary-Thomas@users.noreply.github.com>
2024-09-30 10:38:14 -0400

    Update DB_Init.sql
    Userid 4 updated to supabase

--- DB_Init.sql
+++ DB_Init.sql
@@ -83,7 +83,7 @@ BEGIN;
 		('John', 'Doe', '123456', '555-1234', 'john@example.com', 'ABC123'),
 		('Jane', 'Smith', '654321', '555-5678', 'jane@example.com', 'DEF456'),
 		('Alice', 'Johnson', '987654', '555-9876', 'alice@example.com', 'GHI789'),
-		('Bob', 'Williams', '456789', '555-4321', 'bob@example.com', 'JKL012'),
+		('Bob', 'Williams', '456789', '555-4321', 'bob@example.com', 'JKL012'), -- userid: '122a3c6f-1965-4b26-9fd0-8d71b45cd068'
 		('Emily', 'Brown', '789012', '555-8765', 'emily@example.com', 'MNO345'),
 		('Michael', 'Jones', '321654', '555-2109', 'michael@example.com', 'PQR678'),
 		('Samantha', 'Davis', '456123', '555-6789', 'samantha@example.com', 'STU901'),
@@ -94,11 +94,11 @@ BEGIN;
 	INSERT INTO parking_permits(userID, permit_number, active_status, permit_type, activate_date, expiration_date, campus_location)
 	VALUES
 		(1, 'P1234', TRUE, 'Regular', '2024-03-17', '2025-03-17', 'Brampton Campus'),
-		(3, 'P5678', TRUE, 'Faculty', '2024-03-17', '2025-03-17', 'Trafalgar Campus'),
-		(5, 'P9012', TRUE, 'Student', '2024-03-17', '2025-03-17', 'HMC Campus'),
-		(7, 'P3456', TRUE, 'Visitor', '2024-03-17', '2024-03-18', 'Trafalgar Campus');
+		(2, 'P5678', TRUE, 'Faculty', '2024-03-17', '2025-03-17', 'Trafalgar Campus'),
+		(3, 'P9012', TRUE, 'Student', '2024-03-17', '2025-03-17', 'HMC Campus'),
+		('122a3c6f-1965-4b26-9fd0-8d71b45cd068', 'P3456', TRUE, 'Visitor', '2024-03-17', '2024-03-18', 'Trafalgar Campus'); -- permitid: 61da1f39-63a4-4e61-8663-e11c5ef979bc
 
-	INSERT INTO schedule_blocks(userID, description, dow, start_time, end_time)
+	INSERT INTO schedule_blocks(userID, description, dow, start_time, end_time, block_color)
 	VALUES
 	    (1, 'Class 1', 1, '12:00:00', '14:00:00'),
 	    (1, 'Class 2', 3, '13:00:00', '15:00:00'),
@@ -118,11 +118,11 @@ BEGIN;
 	    (3, 'Study Time', 1, '14:00:00', '17:00:00'),
 	    (3, 'Office Hours', 3, '10:00:00', '12:00:00'),
 	
-	    (4, 'Discussion', 3, '13:00:00', '15:00:00'),
-	    (4, 'Lab Session', 5, '10:00:00', '12:00:00'),
-	    (4, 'Group Study', 1, '16:00:00', '18:00:00'),
-	    (4, 'Meeting', 4, '15:00:00', '17:00:00'),
-	    (4, 'Lecture', 2, '09:00:00', '11:00:00'),
+	    ('122a3c6f-1965-4b26-9fd0-8d71b45cd068', 'Discussion', 3, '13:00:00', '15:00:00', '#9b6134'), -- scheduleid: 7c0fe877-2944-4d85-9351-fd60f6c7e60b
+	    ('122a3c6f-1965-4b26-9fd0-8d71b45cd068', 'Lab Session', 5, '10:00:00', '12:00:00'), -- scheduleid: f17084e5-7f11-4f45-9c1d-14a24107afdc
+	    ('122a3c6f-1965-4b26-9fd0-8d71b45cd068', 'Group Study', 1, '16:00:00', '18:00:00'), -- scheduleid: 5603c2bd-5808-4960-9ad8-20f9785d46af
+	    ('122a3c6f-1965-4b26-9fd0-8d71b45cd068', 'Meeting', 4, '15:00:00', '17:00:00'), -- scheduleid: ec70f4d4-6f55-470e-854a-7d75135915e1
+	    ('122a3c6f-1965-4b26-9fd0-8d71b45cd068', 'Lecture', 2, '09:00:00', '11:00:00'), -- scheduleid: 27317c3d-2f80-4789-9541-97adc5e5f58c
 	
 	    (5, 'Seminar', 2, '15:00:00', '17:00:00'),
 	    (5, 'Discussion', 4, '13:00:00', '15:00:00'),
@@ -165,7 +165,7 @@ BEGIN;
 		(1, false),
 		(2, true),
 		(3, false),
-		(4, false);
+		('61da1f39-63a4-4e61-8663-e11c5ef979bc', false); --group id : 8e023549-9247-434f-b3dc-330dcf894833
 
 	UPDATE users
 	SET groupID = 1
@@ -180,7 +180,7 @@ BEGIN;
 	WHERE userID IN (3,8);
 
 	UPDATE users
-	SET groupID = 4
-	WHERE userID IN (4);
+	SET groupID = '8e023549-9247-434f-b3dc-330dcf894833'
+	WHERE userID IN ('122a3c6f-1965-4b26-9fd0-8d71b45cd068');
 COMMIT;
 	
