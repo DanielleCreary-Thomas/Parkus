ab5f71e89add1f7b50b3c33cf08ad525817d937d
Yunpeng Shan <shanyunp@sheridancollege.ca>
2024-10-27 23:17:20 -0400

    Add cache buster

--- FrontEnd/parkus/src/components/Group/LeaderCard/LeaderCard.js
+++ FrontEnd/parkus/src/components/Group/LeaderCard/LeaderCard.js
@@ -39,7 +39,6 @@ export default function LeaderCard({data, permitData, currUserID}) {
 
     const MemberCard = ({memberData})=>
     {
-        const cacheBustedMemberData = `${memberData['image_proof_url']}?cachebuster=${new Date().getTime()}`;
         console.log("memberData:", memberData)//data for a single member
         const cacheBustedMemberData = `${memberData['image_proof_url']}?cachebuster=${new Date().getTime()}`;
 
