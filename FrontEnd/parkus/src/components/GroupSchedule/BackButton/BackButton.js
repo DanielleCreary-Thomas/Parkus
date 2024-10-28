6f9f4273bbe4797323f8a44138000b1b042ec934
Ram <rameezify98@gmail.com>
2024-10-27 20:49:09 -0400

    refactored spotsharing group schedule ui

--- FrontEnd/parkus/src/components/GroupSchedule/BackButton/BackButton.js
+++ FrontEnd/parkus/src/components/GroupSchedule/BackButton/BackButton.js
@@ -1,14 +1,24 @@
 // src/components/GroupSchedule/BackButton/BackButton.js
 
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import './BackButton.css';
 
 const BackButton = () => {
     const navigate = useNavigate();
+    const location = useLocation();
 
     const handleBack = () => {
-        navigate(-1); // Navigate back to the previous page
+        if (location.state && location.state.fromAvailableGroups) {
+            navigate('/spotsharing', {
+                state: {
+                    showAvailableGroups: true,
+                    availableGroups: location.state.availableGroups,
+                },
+            });
+        } else {
+            navigate('/spotsharing');
+        }
     };
 
     return (
