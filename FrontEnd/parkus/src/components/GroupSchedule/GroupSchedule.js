fa4dfbdfd77897185d540050c3ab2e241a9b0962
Yunpeng Shan <shanyunp@sheridancollege.ca>
2024-10-27 22:01:22 -0400

    merged feature from main to my branch

--- FrontEnd/parkus/src/components/GroupSchedule/GroupSchedule.js
+++ FrontEnd/parkus/src/components/GroupSchedule/GroupSchedule.js
@@ -80,11 +80,8 @@ const GroupSchedule = () => {
     }, [groupId]);
 
     const lightColors = [
-        '#FFCDD2', '#F8BBD0', '#E1BEE7', '#D1C4E9',
-        '#C5CAE9', '#BBDEFB', '#B3E5FC', '#B2EBF2',
-        '#B2DFDB', '#C8E6C9', '#DCEDC8', '#F0F4C3',
-        '#FFF9C4', '#FFECB3', '#FFE0B2', '#FFCCBC',
-        '#D7CCC8', '#CFD8DC',
+        '#C5B6DD', '#A9B7C4', '#A2BBF3', '#8CD3D0',
+        '#AEF1C8', '#E4DEA7', '#E9C8A8', '#D7B0AE'
     ];
 
     const generateClassColorMap = (blocks) => {
